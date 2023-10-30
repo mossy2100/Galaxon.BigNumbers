cb2f1850676f129650656d083d172e8538a7652c
Shaun Moss <shaun@astromultimedia.com>
2023-10-30 11:40:33 +1000

    Just tidying up.

--- BigNumbers/BigComplex/BigComplexConstants.cs
+++ BigNumbers/BigComplex/BigComplexConstants.cs
@@ -11,13 +11,9 @@ public partial struct BigComplex
     /// <inheritdoc/>
     public static BigComplex Tau => new (BigDecimal.Tau);
 
-    /// <summary>
-    /// The golden ratio (φ).
-    /// </summary>
+    /// <summary>The golden ratio (φ).</summary>
     public static BigComplex Phi => new (BigDecimal.Phi);
 
-    /// <summary>
-    /// The natural logarithm of 10.
-    /// </summary>
+    /// <summary>The natural logarithm of 10.</summary>
     public static BigComplex Ln10 => new (BigDecimal.Ln10);
 }
--- BigNumbers/BigDecimal/BigDecimalConstants.cs
+++ BigNumbers/BigDecimal/BigDecimalConstants.cs
@@ -9,9 +9,7 @@ public partial struct BigDecimal
 {
     // ---------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Cached value for e.
-    /// </summary>
+    /// <summary>Cached value for e.</summary>
     private static BigDecimal _e;
 
     /// <inheritdoc/>
@@ -19,15 +17,8 @@ public partial struct BigDecimal
     {
         get
         {
-            if (_e.NumSigFigs == MaxSigFigs)
-            {
-                return _e;
-            }
-
-            if (_e.NumSigFigs > MaxSigFigs)
-            {
-                return RoundSigFigs(_e);
-            }
+            if (_e.NumSigFigs == MaxSigFigs) return _e;
+            if (_e.NumSigFigs > MaxSigFigs) return RoundSigFigs(_e);
 
             // Calculate e:
             _e = Exp(1);
@@ -37,9 +28,7 @@ public partial struct BigDecimal
 
     // ---------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Cached value for π.
-    /// </summary>
+    /// <summary>Cached value for π.</summary>
     private static BigDecimal _pi;
 
     /// <inheritdoc/>
@@ -47,27 +36,20 @@ public partial struct BigDecimal
     {
         get
         {
-            if (_pi.NumSigFigs == MaxSigFigs)
-            {
-                return _pi;
-            }
-
-            if (_pi.NumSigFigs > MaxSigFigs)
-            {
-                return RoundSigFigs(_pi);
-            }
+            if (_pi.NumSigFigs == MaxSigFigs) return _pi;
+            if (_pi.NumSigFigs > MaxSigFigs) return RoundSigFigs(_pi);
 
             _pi = ComputePi();
             return _pi;
         }
     }
 
-    /// <summary>
-    /// Compute π.
+    /// <summary>Compute π.</summary>
+    /// <remarks>
     /// The Chudnovsky algorithm used here was the one used to generate π to 6.2 trillion decimal
     /// places, the current world record.
-    /// See: <see href="https://en.wikipedia.org/wiki/Chudnovsky_algorithm"/>
-    /// </summary>
+    /// <see href="https://en.wikipedia.org/wiki/Chudnovsky_algorithm"/>
+    /// </remarks>
     public static BigDecimal ComputePi()
     {
         // Temporarily increase the maximum number of significant figures to ensure a correct
@@ -91,10 +73,7 @@ public partial struct BigDecimal
             var newSum = sum + M * L / X;
 
             // If adding the new term hasn't affected the sum, we're done.
-            if (sum == newSum)
-            {
-                break;
-            }
+            if (sum == newSum) break;
 
             // Prepare for next iteration.
             sum = newSum;
@@ -116,9 +95,7 @@ public partial struct BigDecimal
 
     // ---------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Cached value for τ.
-    /// </summary>
+    /// <summary>Cached value for τ.</summary>
     private static BigDecimal _tau;
 
     /// <inheritdoc/>
@@ -126,24 +103,15 @@ public partial struct BigDecimal
     {
         get
         {
-            if (_tau.NumSigFigs == MaxSigFigs)
-            {
-                return _tau;
-            }
-
-            if (_tau.NumSigFigs > MaxSigFigs)
-            {
-                return RoundSigFigs(_tau);
-            }
+            if (_tau.NumSigFigs == MaxSigFigs) return _tau;
+            if (_tau.NumSigFigs > MaxSigFigs) return RoundSigFigs(_tau);
 
             _tau = ComputeTau();
             return _tau;
         }
     }
 
-    /// <summary>
-    /// Compute the value of tau (τ), equal to 2 * pi (2π).
-    /// </summary>
+    /// <summary>Compute the value of tau (τ), equal to 2 * pi (2π).</summary>
     /// <returns>The value of τ to the current number of significant figures.</returns>
     public static BigDecimal ComputeTau()
     {
@@ -162,9 +130,7 @@ public partial struct BigDecimal
 
     // ---------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Cached value for φ, the golden ratio.
-    /// </summary>
+    /// <summary>Cached value for φ, the golden ratio.</summary>
     private static BigDecimal _phi;
 
     /// <summary>
@@ -174,24 +140,15 @@ public partial struct BigDecimal
     {
         get
         {
-            if (_phi.NumSigFigs == MaxSigFigs)
-            {
-                return _phi;
-            }
-
-            if (_phi.NumSigFigs > MaxSigFigs)
-            {
-                return RoundSigFigs(_phi);
-            }
+            if (_phi.NumSigFigs == MaxSigFigs) return _phi;
+            if (_phi.NumSigFigs > MaxSigFigs) return RoundSigFigs(_phi);
 
             _phi = ComputePhi();
             return _phi;
         }
     }
 
-    /// <summary>
-    /// Compute the value of phi (φ), the golden ration.
-    /// </summary>
+    /// <summary>Compute the value of phi (φ), the golden ratio.</summary>
     /// <returns>The value of φ to the current number of significant figures.</returns>
     public static BigDecimal ComputePhi()
     {
@@ -210,29 +167,20 @@ public partial struct BigDecimal
 
     // ---------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Cached value for Log(10), the natural logarithm of 10.
+    /// <summary>Cached value for Log(10), the natural logarithm of 10.</summary>
+    /// <remarks>
     /// This value is cached because of it's use in the Log() method. We don't want to have to
     /// recompute Log(10) every time we call Log().
-    /// </summary>
+    /// </remarks>
     private static BigDecimal _ln10;
 
-    /// <summary>
-    /// The natural logarithm of 10.
-    /// </summary>
+    /// <summary>The natural logarithm of 10.</summary>
     public static BigDecimal Ln10
     {
         get
         {
-            if (_ln10.NumSigFigs == MaxSigFigs)
-            {
-                return _ln10;
-            }
-
-            if (_ln10.NumSigFigs > MaxSigFigs)
-            {
-                return RoundSigFigs(_ln10);
-            }
+            if (_ln10.NumSigFigs == MaxSigFigs) return _ln10;
+            if (_ln10.NumSigFigs > MaxSigFigs) return RoundSigFigs(_ln10);
 
             _ln10 = Log(10);
             return _ln10;
