38818bda352eb778dc43af2d47efd346916b7e9b
Shaun Moss <shaun@astromultimedia.com>
2023-11-01 17:03:01 +1000

    Removed unnecessary constants from BigComplex.

--- BigNumbers/BigComplex/BigComplex.Constants.cs
+++ BigNumbers/BigComplex/BigComplex.Constants.cs
@@ -10,10 +10,4 @@ public partial struct BigComplex
 
     /// <inheritdoc/>
     public static BigComplex Tau => new (BigDecimal.Tau);
-
-    /// <summary>The golden ratio (φ).</summary>
-    public static BigComplex Phi => new (BigDecimal.Phi);
-
-    /// <summary>The natural logarithm of 10.</summary>
-    public static BigComplex Ln10 => new (BigDecimal.Ln10);
 }
